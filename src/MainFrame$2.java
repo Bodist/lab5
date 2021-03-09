f2446f2d6101d35d265734eb09f9dbf5ed1bc14f
Bodist <Bgdn1487@yandex.by>
2021-03-09 21:10:19 +0300

    MainFrame

---
+++ src/MainFrame$2.java
@@ -0,0 +1,17 @@
+public class MainFrame$2 {
+}
+package bsu.rfe.java.teacher;
+
+        import java.awt.event.ActionEvent;
+        import javax.swing.AbstractAction;
+
+class MainFrame$2 extends AbstractAction {
+    MainFrame$2(MainFrame var1, String $anonymous0) {
+        super($anonymous0);
+        this.this$0 = var1;
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        MainFrame.access$2(this.this$0).reset();
+    }
+}
